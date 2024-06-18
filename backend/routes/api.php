921670aed2e4bc014037c944289a76fc63e89a95
sochingchun <soching.chun@student.passerellesnumeriques.org>
2024-06-18 18:02:29 +0700

    fixing code on branch create_category #GL-21

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -28,7 +28,6 @@ Route::get('/post/list', [PostController::class, 'index'])->middleware('auth:san
 // crud on categories
 Route::get('/categories/list', [CategoryController::class, 'index']);
 Route::post('/create/category', [CategoryController::class, 'store']);
-<<<<<<< HEAD
 
 
 
@@ -43,6 +42,4 @@ Route::middleware('auth:sanctum')->group(function () {
 });
 
 // Other routes that don't require authentication
-=======
 Route::put('/update/category/{id}', [CategoryController::class, 'update']);
->>>>>>> e3ca59eab26f5519a70a02ec4965a3e0975a945d
