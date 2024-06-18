da99b32cdbbdbc211d59980609f3bbd745b91b7f
sochingchun <soching.chun@student.passerellesnumeriques.org>
2024-06-18 15:45:26 +0700

    get categories and create categories #GL-21

---
+++ backend/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+use Exception;
+use App\Http\Resources\CategoryResource;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $category = Category::all();
+        $category = CategoryResource::collection($category);
+        return response()->json(['data' => $category, 'message' => 'You can get the categories list'], status:200);
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {   
+        $category_name= $request->category_name;
+        $category = new Category();
+
+        $category->category_name = $category_name;
+        try {
+            $category->save();
+            return response()->json(['data' => $category, 'message' => 'You can store the category'], status:200);
+        }catch(Exception $error){
+            return response()->json(['data' => $error, 'message' => 'You can not store the category'], status:400);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\PostController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/me', [AuthController::class, 'index'])->middleware('auth:sanctum');
 Route::get('/post/list', [PostController::class, 'index'])->middleware('auth:sanctum');
+
+// crud on categories
+Route::get('/categories/list', [CategoryController::class, 'index']);
+Route::post('/create/category', [CategoryController::class, 'store']);
