71a39d9f2e4d405d4ff6c0eae924bb26e2e65b0c
thona ngon <thona ngon@example.com>
2024-06-24 20:15:39 +0700

    update card

--- frontend/src/LoukDo.vue
+++ frontend/src/LoukDo.vue
@@ -1,7 +1,6 @@
 <template>
     <div class="container">
       <nav-bar/>
-      <Card></Card>
       <body>
         <router-view/>
       </body>
@@ -13,12 +12,10 @@
 // import navbar from "@/components/navbars/NavBar.vue"
 import NavBar from '@/Components/navbars/NavBar.vue';
 import Footer from '@/Components/footer/FooTer.vue';
-import Card from '@/Components/Card/CardComponent.vue';
 export default {
   components:{
     NavBar,
     Footer,
-    Card
   }
 
 }
