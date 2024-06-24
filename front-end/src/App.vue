47d428c1fb3e363882116074d74343ad0bdd70f4
thona ngon <thona ngon@example.com>
2024-06-24 12:54:59 +0700

    card component #GL-55

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <!-- <NavBars></NavBars> -->
+    <LoukDo></LoukDo>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LoukDo from './views/LoukDo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoukDo
   }
 }
 </script>
--- front-end/src/LoukDo.vue
+++ front-end/src/LoukDo.vue
@@ -1,15 +1,58 @@
 <template>
-  <div>
-    This is our Home page
+  <div class="container">
+    <header class="jumbotron jumbotron-fluid text-center p-5">
+      <div class="container">
+        <div class="d-flex align-items-center justify-content-center gap-5 font-size-5">
+          <a href="#"><Table>TOP |</Table></a>
+          <a href="#"><Table>BOTTOM |</Table></a>
+          <a href="#"><Table>SELE...</Table></a>
+        </div>
+      </div>
+    </header>
+    <CardComponent></CardComponent>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script >
+  import CardComponent from './components/Card/CardComponent.vue'
+  export default ({
+    name: 'HomeView',
+    components: {
+      CardComponent
+    },
+    data() {
+      return {
+        products :[
+          {
+            id: 1,
+            name:'thonal',
+            title: 'Example Clothing',
+            image: 'https://images.nightcafe.studio/jobs/8gONOFn1ruXBxrx9Jsc3/8gONOFn1ruXBxrx9Jsc3--1--9m9d2_2x.jpg?tr=w-1600,c-at_max'
+          },
+          {
+            id: 2,
+            name:'thonal',
+            title: 'Example Clothing',
+            image: 'https://images.nightcafe.studio/jobs/8gONOFn1ruXBxrx9Jsc3/8gONOFn1ruXBxrx9Jsc3--1--9m9d2_2x.jpg?tr=w-1600,c-at_max'
+          },
+          {
+            id: 3,
+            name:'thonal',
+            title: 'Example Clothing',
+            image: 'https://images.nightcafe.studio/jobs/8gONOFn1ruXBxrx9Jsc3/8gONOFn1ruXBxrx9Jsc3--1--9m9d2_2x.jpg?tr=w-1600,c-at_max'
+          },
+          {
+            id: 4,
+            name:'thonal',
+            title: 'Example Clothing',
+            image: 'https://images.nightcafe.studio/jobs/8gONOFn1ruXBxrx9Jsc3/8gONOFn1ruXBxrx9Jsc3--1--9m9d2_2x.jpg?tr=w-1600,c-at_max'
+          }
+        ]
+      }
+    }
+  });
 </script>
 
-<style>
-
+<style scoped>
+/* Add any component-specific styles here */
 </style>
