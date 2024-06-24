17abbab8445f3228dcf3190434367e900443ed2c
thona ngon <thona ngon@example.com>
2024-06-24 08:45:22 +0700

    structure code

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,15 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePageVue></HomePageVue>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HomePagevue from './views/HomePageVue.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePagevue
   }
 }
 </script>
