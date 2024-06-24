0b38e3471804305c1960c55ea6fa2206bec19b5d
sochingchun <soching.chun@student.passerellesnumeriques.org>
2024-06-24 14:57:32 +0700

    I did on view login for user #GL-47

--- front-end/src/LoukDo.vue
+++ front-end/src/LoukDo.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="container">
       <nav-bar/> 
+      <login />
     </div>
+
 </template>
 
 <script>
 // import navbar from "@/components/navbars/NavBar.vue"
 import NavBar from './components/navbars/NavBar.vue'
+import login from './components/login.vue'
 export default {
   components:{
-    NavBar
+    NavBar,
+    login
   }
 
 }
---
+++ front-end/src/components/login.vue
@@ -0,0 +1,35 @@
+<template>
+<form>
+  <div class="container bg-light p-4 rounded shadow w-50">
+    <div class="mb-3 text-center">
+      <h3>Login account</h3>
+      <span>Have a nice day!</span>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label fw-bold" style="padding-top: 10px;">Email address</label>
+      <input type="email" class="form-control fw-bold" >
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label fw-bold" style="padding-top: 10px;">Password</label>
+      <div class="d-flex align-items-center">
+        <input type="password" class="form-control fw-bold flex-grow-1" >
+        <button type="submit" class="btn btn-primary ms-3">Submit</button>
+      </div>
+    </div>
+    <div>
+      <a href="" style="text-decoration: none;">Did you forget password?</a>
+    </div>
+  </div>
+</form>
+</template>
+
+<script>
+export default {
+    name: "LoginComponent",
+
+}
+</script>
+
+<style>
+
+</style>
