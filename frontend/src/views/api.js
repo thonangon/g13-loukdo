a7a0b5b4987f1d2bcbc71eec7de2db92d495be5e
Kemleanglor <kemleang.lor@student.passerellesnumeriques.org>
2024-06-26 23:25:44 +0700

    resolve error on UserProfile

--- frontend/src/views/api.js
+++ frontend/src/views/api.js
@@ -1,10 +1,7 @@
-// src/api.js
 import axios from 'axios';
-
 const API_URL = 'http://127.0.0.1:8000/api';
-
 export default {
-  listProdcut(data) {
-    return axios.get(`${API_URL}/products/list`, data);
-  },
+    viewProfile(data) {
+        return axios.get(${API_URL}/user/{id}, data);
+      },
 };
