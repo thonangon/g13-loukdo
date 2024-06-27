ec844957ddda3a7d03fcce15491312ae10ea462a
Kemleanglor <kemleang.lor@student.passerellesnumeriques.org>
2024-06-27 20:06:22 +0700

    commit code

--- frontend/src/views/api.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-const API_URL = 'http://127.0.0.1:8000/api';
-export default {
-    viewProfile(data) {
-        return axios.get(${API_URL}/user/{id}, data);
-      },
-};
