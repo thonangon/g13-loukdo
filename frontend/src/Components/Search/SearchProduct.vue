6ac02bc7ee65d7bd6e2301eb41a33a32db6349f8
SENRIN-SIM <senrin.sim@student.passerellesnumeriques.org>
2024-06-30 15:15:35 +0700

    [feat] create and show comment to each product GL-51

--- frontend/src/Components/Search/SearchProduct.vue
+++ frontend/src/Components/Search/SearchProduct.vue
@@ -5,7 +5,7 @@
                 <form action="https://www.google.com/search" method="GET" target="_blank" class="input-group">
                     <input type="text" class="form-control rounded-pill" name="q" placeholder="Search Google..." aria-label="Search Google">
                     <button class="btn btn-primary rounded-pill" type="submit">
-                        Search
+                        Loukdo
                     </button>
                 </form>
             </div>
