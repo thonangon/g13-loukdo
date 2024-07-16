b41aab61d39d9f33ca541e28353a5caa74b867e0
Kemleanglor <kemleang.lor@student.passerellesnumeriques.org>
2024-07-17 02:52:51 +0700

    count user that created store #GL-109

--- frontend/src/views/Admin/Auth/UserCreateStore.vue
+++ frontend/src/views/Admin/Auth/UserCreateStore.vue
@@ -5,15 +5,21 @@
         <tr class="text-center">
           <th scope="col">Id</th>
           <th scope="col">Name</th>
-          <th scope="col">Email</th> <!-- Update the table header to display "Creator Name" -->
+          <th scope="col">Email</th>
+          <th scope="col">StoreName</th>
+          <th scope="col">Image</th>
           <th scope="col" style="width: 10%;">Actions</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="user in usersstore" :key="user.id" class="text-center">
-          <td>{{ }}</td>
-          <td>{{ }}</td> <!-- Display the store name -->
-          <td>{{ }}</td> <!-- Display the creator's name here -->
+        <tr v-for="(user, index) in users" :key="index" class="text-center">
+          <td>{{ index + 1 }}</td>
+          <td>{{ user.user.name }}</td>
+          <td>{{ user.user.email }}</td>
+          <td>{{ user.name }}</td>
+          <td>
+            <img :src="user.image_url" alt="Store Image" class="img-fluid" style="height: 50px; width: 50px;" />
+          </td>
           <td>
             <button class="btn btn-danger mb-2">Block</button>
           </td>
@@ -24,5 +30,29 @@
 </template>
 
 <script>
+import api from "@/views/api";
 
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  mounted() {
+    this.getStores();
+  },
+  methods: {
+    async getStores() {
+      let response = await api.getStores();
+      this.users = response.data.data.map(store => ({
+        user: store.user,
+        name: store.name,
+        image_url: store.image_url,
+      }));
+    },
+  },
+};
 </script>
+
+<style>
+</style>
